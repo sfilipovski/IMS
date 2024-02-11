733bf9cc12086dc6d3051425231d2071a402eb31
sfilipovski <filipovskisergej@gmail.com>
2024-02-11 06:18:34 +0100

    fix-dbcontext: fix adding scaffolded views

---
+++ IMS.Repository/ApplicationDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Repository;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-IMS.Web-b73b3d3e-c222-4c28-be39-9a5cc77ae80c;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
