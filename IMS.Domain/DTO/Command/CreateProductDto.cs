62f238a0aed69f0ca751a7cb31e006f57312bbd4
sfilipovski <filipovskisergej@gmail.com>
2024-02-11 01:46:52 +0100

    add product dtos

---
+++ IMS.Domain/DTO/Command/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace IMS.Domain.DTO.Command;
+
+public class CreateProductDto
+{
+    public string ProductName { get; set; }
+    public string? ProductDescription { get; set; }
+    public string? ProductSKU { get; set; }
+    public string? ProductImageUrl { get; set; }
+    public double ProductPrice { get; set; }
+    public int? CategoryId { get; set; }
+    public int? SupplierId { get; set; }
+}
---
+++ IMS.Domain/DTO/Query/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain.DTO.Query;
+
+public class ProductDto
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public string ProductSKU { get; set; }
+    public string ProductImageUrl { get; set; }
+    public double ProductPrice { get; set; }
+    public int? CategoryId { get; set; }
+    public int? SupplierId { get; set; }
+}
